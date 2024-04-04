469def0a63393a7c3ed5794a099d0f9a9e0575aa
claveille hugo <ch204830@ferrari.unice.fr>
2024-04-04 10:04:54 +0200

    cpt

--- api/db.php
+++ api/db.php
@@ -5,7 +5,7 @@ $password = "\$ll5Yn793";
 $dbname = "db";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname), ;
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
