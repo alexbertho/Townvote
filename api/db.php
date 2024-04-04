bf2a13c63c98b71aef5cde3ddf68e2a16b35c064
claveille hugo <ch204830@ferrari.unice.fr>
2024-04-04 10:08:52 +0200

    ctrept

--- api/db.php
+++ api/db.php
@@ -19,7 +19,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-      echo $row['id'] . " - login: " . $row['login'] . " - password: " . $row['password'] . "<br>";
+      echo $row['id'] . " - login: " . $row['login'] . " - password: " . $row['pass'] . "<br>";
     }
   } else {
     echo "0 results";
