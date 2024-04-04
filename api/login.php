d5d7b96b6ae3855570f3ab6c6e62b37ba4950942
claveille hugo <ch204830@ferrari.unice.fr>
2024-04-04 09:54:40 +0200

    cpt

--- api/login.php
+++ api/login.php
@@ -8,13 +8,27 @@ if (isset($_SESSION['user_id'])) {
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     require_once 'db.php';
+
     $username = $_GET['username'];
     $password = $_GET['password'];
+    echo "<br>";
     echo $username;
     echo "<br>";
     echo $password;
     
+    $sql = "SELECT * FROM users WHERE username = '$username'";
 
+    
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        if (password_verify($password, $row['password'])) {
+            $_SESSION['user_id'] = $row['id'];
+            header("Location: index.php");
+            exit();
+        }
+    }
+    $error = 'Invalid username or password';
 }
 
 // if ($_SERVER['REQUEST_METHOD'] === 'POST') {
