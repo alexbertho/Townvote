c52b72beb789243e1786fd7dc66d42bd090c78f9
claveille hugo <ch204830@ferrari.unice.fr>
2024-04-04 09:35:01 +0200

    ajout login

---
+++ api/login.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+
+if (isset($_SESSION['user_id'])) {
+    header("Location: index.php");
+    exit();
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    echo 'GET';
+    $username = $_GET['username'];
+    $password = $_GET['password'];
+    
+    require_once 'db.php';
+
+}
+
+// if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+//     require_once 'db.php';
+//     $username = $_POST['username'];
+//     $password = $_POST['password'];
+    
+//     $sql = "SELECT * FROM users WHERE username = '$username'";
+
+    
+
+//     $result = $conn->query($sql);
+//     if ($result->num_rows > 0) {
+//         $row = $result->fetch_assoc();
+//         if (password_verify($password, $row['password'])) {
+//             $_SESSION['user_id'] = $row['id'];
+//             header("Location: index.php");
+//             exit();
+//         }
+//     }
+//     $error = 'Invalid username or password';
+// }
+
+
+?>
