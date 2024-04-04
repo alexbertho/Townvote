2682a2619679068bfa92884f2fc0e870cc963148
claveille hugo <ch204830@ferrari.unice.fr>
2024-04-04 10:06:31 +0200

    cpt

--- api/db.php
+++ api/db.php
@@ -15,7 +15,16 @@ echo "Connected successfully";
 
 
 $sql = "SELECT * FROM clients";
+
 $result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+      echo "id: " . $row["id"]. " - Name: " . $row["name"]. " " . $row["email"]. "<br>";
+    }
+  } else {
+    echo "0 results";
+  }
 echo "---<br>";
 echo $result;
 echo "<br>----";
