f07389873a81aa23dd3325ade6f49cd56d1a39ee
claveille hugo <ch204830@ferrari.unice.fr>
2024-04-11 09:24:52 +0200

    maj auto

--- api/login.php
+++ api/login.php
@@ -5,33 +5,6 @@ if (isset($_SESSION['user_id'])) {
     header("Location: index.php");
     exit();
 }
-
-// if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    
-
-//     $sql = "SELECT * FROM clients WHERE login = '$username'";
-
-//     $result = $conn->query($sql);
-//     if ($result->num_rows > 0) {
-//         // output data of each row
-//         while($row = $result->fetch_assoc()) {
-//         echo $row['id'] . " - login: " . $row['login'] . " - password: " . $row['pass'] . "<br>";
-//         }
-//     } else {
-//         echo "0 results";
-//     }
-//     if ($result->num_rows == 1) {
-//         $row = $result->fetch_assoc();
-//         if ($password == $row['pass']) {
-//             $_SESSION['user_id'] = $row['id'];
-//             header("Location: index.php");
-//             exit();
-//         }
-//     } else {
-//         $error = 'Invalid username or password';
-//     }
-// }
-
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (isset($_POST['username']) && isset($_POST['password'])) {
         
@@ -45,6 +18,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     $sql = "SELECT * FROM clients WHERE login = '$username'";
     $result = $conn->query($sql);
+    $data = [];
     if ($result->num_rows == 1) { // Si un utilisateur a été trouvé
         $row = $result->fetch_assoc();
         if ($password == $row['pass']) { // Si le mot de passe est correct
@@ -66,13 +40,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         ];
     }
 
-    $jsonData = json_encode($data);
 
     // Set the content type header
     header('Content-Type: application/json');
 
     // Output the JSON data
-    echo $jsonData;
+    echo json_encode($data);
 }
 ?>
 
