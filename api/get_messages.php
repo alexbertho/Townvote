2ebf89eed296d7d79b3f8f7e4ad8d9fd3062eca6
CLAVEILLE Hugo <hugo.claveille@etu.univ-cotedazur.fr>
2024-05-01 18:31:40 +0200

    test

--- api/get_messages.php
+++ api/get_messages.php
@@ -1,37 +1,6 @@
 <?php
 session_start();
-if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    echo "<h1>Messages</h1>"
-    if (isset($_GET['ag_id']) or true) {
-        $ag_id = $_GET['ag_id'];
-    } else {
-        // http_response_code(403);
-        // exit(); // Terminer le script pour éviter toute exécution supplémentaire
-    }
-    require_once 'db.php';
+echo "test";
 
-    $sql = "SELECT * FROM `message` WHERE `ag_id`=1"
-    $result = $conn->query($sql);
-    $data = array();
 
-    echo "<h1>Messages</h1>"
-    
-    if ($result->num_rows == 1) {
-        echo "<ul>"
-        foreach ($result as $row) {
-            echo "<li>" . $row['message'] . "</li>";
-        }
-        echo "</ul>"
-
-    } else if ($result->num_rows == 0) {
-        
-    }
-    
-    header('Content-Type: application/json');
-    echo json_encode($data);
-    $conn->close();
-
-}
 ?>
-
-
