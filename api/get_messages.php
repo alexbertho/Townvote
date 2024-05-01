f8534ac92bdcf8e679fe32527ee6cce29be331c2
CLAVEILLE Hugo <hugo.claveille@etu.univ-cotedazur.fr>
2024-05-01 18:34:10 +0200

    test

--- api/get_messages.php
+++ api/get_messages.php
@@ -4,3 +4,16 @@ echo "test";
 
 
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Messages</title>
+</head>
+<body>
+    <h1>Messages</h1>
+    <?php
+    // Your code to retrieve and display messages goes here
+    ?>
+</body>
+</html>
