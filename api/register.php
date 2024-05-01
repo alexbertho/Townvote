06341f9c6eeefb2997f37ca4007280fc701336d3
CLAVEILLE Hugo <hugo.claveille@etu.univ-cotedazur.fr>
2024-05-01 18:14:12 +0200

    premier test register

--- api/register.php
+++ api/register.php
@@ -1,52 +1,46 @@
 <?php
 session_start();
-
-if (isset($_SESSION['user_id'])) {
-    header("Location: index.php");
-    exit();
-}
-
-if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    if (isset($_GET['username']) && isset($_GET['password'])) {
-        $username = $_GET['username'];
-        $password = $_GET['password'];
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['username']) && isset($_POST['password'])) {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
     } else {
         http_response_code(403);
         exit(); // Terminer le script pour éviter toute exécution supplémentaire
     }
     require_once 'db.php';
 
-
-    echo "<br> username:";
-    echo $username;
-    echo "<br> password:";
-    echo $password;
-    echo "<br>";
-    echo "<br>";
-    
     $sql = "SELECT * FROM clients WHERE login = '$username'";
-
     $result = $conn->query($sql);
-    if ($result->num_rows > 0) {
-        // output data of each row
-        while($row = $result->fetch_assoc()) {
-        echo $row['id'] . " - login: " . $row['login'] . " - password: " . $row['pass'] . "<br>";
+    $data = array();
+
+    
+    if ($result->num_rows == 1) { // Si un utilisateur a été trouvé
+        
+        $data = [
+            'success' => false,
+            'message' => 'Username already exists'
+        ];
+    } else if ($result->num_rows == 0) { // Si aucun utilisateur n'a été trouvé
+        $sql = "INSERT INTO clients (login, password) VALUES ('$username', '$password')";
+        if ($conn->query($sql) === TRUE) {
+            $data = [
+                'success' => true,
+                'message' => 'User registered successfully'
+            ];
+        } else {
+            $data = [
+                'success' => false,
+                'message' => 'Error: ' . $sql . '<br>' . $conn->error
+            ];
         }
-    } else {
-        echo "0 results";
     }
-    if ($result->num_rows == 1) {
-        $row = $result->fetch_assoc();
-        if ($password == $row['pass']) {
-            $_SESSION['user_id'] = $row['id'];
-            header("Location: index.php");
-            exit();
-        }
-    } else {
-        $error = 'Invalid username or password';
-    }
+    
+    header('Content-Type: application/json');
+    echo json_encode($data);
+    $conn->close();
+
 }
-
-
-
 ?>
+
+
