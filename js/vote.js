522dc43b2c5bba0426873077b569e5b790118acf
CLAVEILLE Hugo <hugo.claveille@etu.univ-cotedazur.fr>
2024-05-15 22:06:56 +0200

    a

--- js/vote.js
+++ js/vote.js
@@ -5,6 +5,31 @@ document.addEventListener("DOMContentLoaded", function() {
   const ctx = graphCanvas.getContext('2d');
   let voteCounts = [];
 
+  
+  // Initialize vote counts
+  voteCounts = [];
+
+  fetch('api/get_votes.php?vote_id=1') // Fetch vote data
+    .then(response => response.json())
+    .then(data => {
+      // console.log(data);
+      // console.log(data['voteCount']);
+      for (var i in data['voteCount']) {
+        voteCounts[i] = (data['voteCount'][i]);
+      }
+
+      for (var key in data['choix']) {
+        const li = document.createElement('li');
+        li.textContent = data['choix'][key]['desc'];
+        li.id = data['choix'][key]['id'];
+        optionsList.appendChild(li);
+      }
+
+      // console.log('vote counts:', voteCounts);
+    updateGraph();
+  });
+
+
   // Add event listeners to list items
   Array.prototype.forEach.call(optionsList.children, (li, index) => {
     li.addEventListener('click', () => {
