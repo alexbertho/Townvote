3a2c70d1d30533e1813c3ffee342c8ac997396e5
CLAVEILLE Hugo <hugo.claveille@etu.univ-cotedazur.fr>
2024-05-15 18:48:56 +0200

    a

--- api/get_messages.php
+++ api/get_messages.php
@@ -1,21 +1,46 @@
 <?php
+// Path: api/get_messages.php
+// Permet de récupérer les messages d'un groupe
+// https://claveille.web-edu.fr/api/get_messages.php?ag_id=1
 session_start();
 if ($_SESSION['user_id'] == null) {
     header("Location: login.php");
     exit();
 }
+$user_id = $_SESSION['user_id'];
+
+function a_acces($conn, $user_id, $ag_id) {
+    $stmt = $conn->prepare("SELECT * FROM user_ag WHERE user_id = ? AND ag_id = ?");
+    $stmt->bind_param("ii", $user_id, $ag_id);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $stmt->close();
+    if ($result->num_rows == 1) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+
+
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     if (isset($_GET['ag_id'])) {
         $ag_id = $_GET['ag_id'];
     } else {
         echo "Erreur: ag_id non défini";
-        // http_response_code(403);
-        // exit(); // Terminer le script pour éviter toute exécution supplémentaire
+        http_response_code(403);
+        exit(); // Terminer le script pour éviter toute exécution supplémentaire
     }
     require_once 'db.php';
 
-    // ! verifier si l'utilisateur est connecté et a accès aux messages
+    if (!a_acces($conn, $user_id, $ag_id)) {
+        echo "Vous n'avez pas accès à ce groupe";
+        exit();
+    }
+    
 
     $sql = "SELECT * FROM `message` INNER JOIN `users` ON `message`.`user_id` = `users`.`id` WHERE `ag_id`='$ag_id'";
     $result = $conn->query($sql);
