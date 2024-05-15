12f0117d40cb08297b73e1273f1c4e947296a70c
CLAVEILLE Hugo <hugo.claveille@etu.univ-cotedazur.fr>
2024-05-15 23:31:27 +0200

    a

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
         $html = '';
         foreach ($buildings as $building) {
             $id = $building['id'];
-            $id = 1
+            $id = 1;
             // $desciption = $building['description'];
             $adresse = $building['adresse'];
             $html .= "
