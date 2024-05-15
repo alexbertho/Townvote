f02e9a6bc53b02f3c171a9d87920238f15154d39
CLAVEILLE Hugo <hugo.claveille@etu.univ-cotedazur.fr>
2024-05-15 22:58:09 +0200

    a

--- api/send_message.php
+++ api/send_message.php
@@ -10,10 +10,10 @@ $user_id = $_SESSION['user_id'];
 
 require_once 'db.php';
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (isset($_POST['message']) && isset($_POST['vote_id'])) {
-        $message = $_POST['message'];
-        $vote_id = $_POST['vote_id'];
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    if (isset($_GET['message']) && isset($_GET['vote_id'])) {
+        $message = $_GET['message'];
+        $vote_id = $_GET['vote_id'];
     } else {
         http_response_code(403);
         exit();
@@ -24,16 +24,16 @@ $stmt = $conn->prepare("SELECT * FROM ag WHERE id = ? AND id IN (SELECT ag_id FR
 $stmt->bind_param("ii", $ag_id, $user_id);
 $stmt->execute();
 $result = $stmt->get_result();
-// if ($result->num_rows == 0) {
-//     $data = [
-//         'success' => false,
-//         'message' => 'Vous n\'avez pas accès à ce vote'
-//     ];
-//     header('Content-Type: application/json');
-//     echo json_encode($data);
-//     $conn->close();
-//     exit();
-// }
+if ($result->num_rows == 0) {
+    $data = [
+        'success' => false,
+        'message' => 'Vous n\'avez pas accès à ce vote'
+    ];
+    header('Content-Type: application/json');
+    echo json_encode($data);
+    $conn->close();
+    exit();
+}
 
 
 $stmt = $conn->prepare("INSERT INTO message (message, vote_id) VALUES (?, ?)");
